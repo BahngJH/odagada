56d1143a2a642124ca9de1a1f46a84f9195d7ff8
sinna94 <sinna94@naver.com>
2019-02-28 14:44:26 +0900

    carpoolRegister - 생성
    carpool register view 로 이동하는 메소드

--- odagada/src/main/java/com/spring/odagada/carpool/controller/CarpoolController.java
+++ odagada/src/main/java/com/spring/odagada/carpool/controller/CarpoolController.java
@@ -1,8 +1,16 @@
 package com.spring.odagada.carpool.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CarpoolController {
 
+	@RequestMapping("/carpool/register")
+	public ModelAndView carpoolRegister() {
+		ModelAndView mav = new ModelAndView("/carpool/register");	
+		
+		return mav;
+	}
 }
