d02ba97d49fafe14ae66bb0c8e361674f258d055
sinna94 <sinna94@naver.com>
2019-03-04 13:42:28 +0900

    Option.java - 생성
    옵션 vo 클래스 생성

---
+++ odagada/src/main/java/com/spring/odagada/carpool/model/vo/Option.java
@@ -0,0 +1,98 @@
+package com.spring.odagada.carpool.model.vo;
+
+public class Option {
+	private String animal;
+	private String smoking;
+	private String teenage;
+	private String talking;
+	private String music;
+	private String food;
+	private String baggage;
+	private String gender;
+	private int seatcount;
+	
+	public Option() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getAnimal() {
+		return animal;
+	}
+
+	public void setAnimal(String animal) {
+		this.animal = animal;
+	}
+
+	public String getSmoking() {
+		return smoking;
+	}
+
+	public void setSmoking(String smoking) {
+		this.smoking = smoking;
+	}
+
+	public String getTeenage() {
+		return teenage;
+	}
+
+	public void setTeenage(String teenage) {
+		this.teenage = teenage;
+	}
+
+	public String getTalking() {
+		return talking;
+	}
+
+	public void setTalking(String talking) {
+		this.talking = talking;
+	}
+
+	public String getMusic() {
+		return music;
+	}
+
+	public void setMusic(String music) {
+		this.music = music;
+	}
+
+	public String getFood() {
+		return food;
+	}
+
+	public void setFood(String food) {
+		this.food = food;
+	}
+
+	public String getBaggage() {
+		return baggage;
+	}
+
+	public void setBaggage(String baggage) {
+		this.baggage = baggage;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public int getSeatcount() {
+		return seatcount;
+	}
+
+	public void setSeatcount(int seatcount) {
+		this.seatcount = seatcount;
+	}
+
+	@Override
+	public String toString() {
+		return "Option [animal=" + animal + ", smoking=" + smoking + ", teenage=" + teenage + ", talking=" + talking
+				+ ", music=" + music + ", food=" + food + ", baggage=" + baggage + ", gender=" + gender + ", seatcount="
+				+ seatcount + "]";
+	}
+	
+	
+}
