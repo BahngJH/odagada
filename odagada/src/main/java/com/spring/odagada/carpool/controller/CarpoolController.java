8c042c4d735ee2c866ea9cbe495378473deab866
JUNGHA <JUNGHA@192.168.0.26>
2019-03-08 14:39:58 +0900

    검색한 옵션값 받아서 requestDispatcher로 던집

--- odagada/src/main/java/com/spring/odagada/carpool/controller/CarpoolController.java
+++ odagada/src/main/java/com/spring/odagada/carpool/controller/CarpoolController.java
@@ -1,10 +1,14 @@
 package com.spring.odagada.carpool.controller;
 
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
@@ -15,8 +19,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.spring.odagada.carpool.model.service.CarpoolService;
-import com.spring.odagada.carpool.model.vo.Carpool;
 import com.spring.odagada.carpool.model.vo.CarOption;
+import com.spring.odagada.carpool.model.vo.Carpool;
 import com.spring.odagada.driver.model.service.DriverService;
 import com.spring.odagada.driver.model.vo.Driver;
 import com.spring.odagada.member.model.vo.Member;
@@ -110,7 +114,12 @@ public class CarpoolController {
 		return mav;
 	}
 	@RequestMapping("/carpool/searchEnd.do")
-	public ModelAndView wcarpoolSearchEnd(String kmNum,String memberNum,String startSearch, String endSearch, String startDate, String startLon, String startLat, String endLon, String endLat){
+	public ModelAndView carpoolSearchEnd(String kmNum,
+			String memberNum,String startSearch, 
+			String endSearch, String startDate, 
+			String startLon, String startLat, 
+			String endLon, String endLat)
+	{
 		ModelAndView mav = new ModelAndView();
 		
 		Map<String,String> m = new HashMap<String, String>();
@@ -122,7 +131,7 @@ public class CarpoolController {
 		m.put("destLong",endLon);
 		m.put("destLat",endLat);
 		m.put("kmNum", kmNum);
-		
+		l.debug(startLon + " : " + startLat+" : "+endLon+":"+ endLat);
 		//반경 구하기(default3)
 		
 		List<Carpool> carlist = service.selectCarpoolList(m);
@@ -132,9 +141,7 @@ public class CarpoolController {
 		
 		mav.addObject("cList",carlist);
 		mav.addObject("dList",driverList);
-		mav.addObject("startSearch",startSearch);
-		mav.addObject("endSearch",endSearch);
-		mav.addObject("startDate",startDate);
+		mav.addObject("search", m);
 		mav.setViewName("carpool/searchEnd");
 		return mav;
 		
@@ -146,5 +153,98 @@ public class CarpoolController {
 		
 		mav.setViewName("carpool/oneSearch");
 		return mav;
+	}
+	
+	@RequestMapping("/carpool/searchOption")
+	public void carpoolSearchOption(HttpServletRequest req,
+			HttpServletResponse res,
+			CarOption option,
+			String kmNum,
+			String startLong,
+			String startLat,
+			String destLat,
+			String destLong) throws ServletException, IOException 
+	{
+		l.debug(option.toString());
+		ModelAndView mav = new ModelAndView();
+		Map<String,String> map=new HashMap();
+		if(option.getAnimal().equals("false")) {
+			option.setAnimal("N");
+			map.put("animal", option.getAnimal());
+		}
+		else {
+			option.setAnimal("Y");
+			map.put("animal", option.getAnimal());
+		}
+			
+		if(option.getBaggage().equals("false")) {
+			option.setBaggage("N");
+			map.put("baggage", option.getBaggage());
+		}
+		else {
+			option.setBaggage("Y");
+			map.put("baggage", option.getBaggage());
+		}
+		if(option.getFood().equals("false")) {
+			option.setFood("N");
+			map.put("food", option.getFood());
+		}
+		else {
+			option.setFood("Y");
+			map.put("food", option.getFood());
+		}
+		if(option.getSmoking().equals("false")) {
+			option.setSmoking("N");
+			map.put("smoking", option.getSmoking());
+		}
+		else {
+			option.setSmoking("Y");
+			map.put("smoking", option.getSmoking());
+		}
+		if(option.getTeenage().equals("false")) {
+			option.setTeenage("N");
+			map.put("teenage", option.getTeenage());
+		}
+		else {
+			option.setTeenage("Y");
+			map.put("teenage", option.getTeenage());
+		}
+		if(option.getTalking().equals("false")) {
+			option.setTalking("N");
+			map.put("talking", option.getTalking());
+		}
+		else {
+			option.setTalking("Y");
+			map.put("talking", option.getTalking());
+		}
+		if(option.getMusic().equals("false")) {
+			option.setMusic("N");
+			map.put("music", option.getMusic());
+		}
+		else {
+			option.setMusic("Y");
+			map.put("music", option.getMusic());
+		}
+		
+		map.put("animal", option.getAnimal());
+		map.put("baggage", option.getBaggage());
+		map.put("food", option.getFood());
+		map.put("smoking", option.getSmoking());
+		map.put("teenage", option.getTeenage());
+		map.put("talking", option.getTalking());
+		map.put("music", option.getMusic());
+		map.put("seatcount",Integer.toString(option.getSeatcount()));
+		map.put("gender", option.getGender());
+		map.put("kmNum", kmNum);
+		map.put("startLat", startLat);
+		map.put("startLong", startLong);
+		map.put("destLat", destLat);
+		map.put("destLong", destLong);
+		
+		l.debug(map.toString());
+		
+		List<Map<String,String>> cList = service.selectCarOptionList(map);
+		req.setAttribute("cList", cList);
+		req.getRequestDispatcher("/WEB-INF/views/carpool/search-fixed.jsp").forward(req, res);
 	}
 }
