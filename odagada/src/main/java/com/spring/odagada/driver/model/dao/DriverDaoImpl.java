d0d44833e69e06ad03a4aa8aecedd25c1fdb4c33
newna <rladbsk1921@hanmail.net>
2019-03-11 13:19:49 +0900

    드라이버 가입, 관리자 계정에서 신청리스트 출력까지 완료.

--- odagada/src/main/java/com/spring/odagada/driver/model/dao/DriverDaoImpl.java
+++ odagada/src/main/java/com/spring/odagada/driver/model/dao/DriverDaoImpl.java
@@ -1,12 +1,15 @@
 package com.spring.odagada.driver.model.dao;
 
 import java.util.List;
+import java.util.Map;
 
+import org.apache.ibatis.session.RowBounds;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.spring.odagada.driver.model.vo.Driver;
+import com.spring.odagada.driver.model.vo.carImage;
 
 @Repository
 public class DriverDaoImpl implements DriverDao {
@@ -17,5 +20,32 @@ public class DriverDaoImpl implements DriverDao {
 	public Driver selectOne(int memberNum) {
 		return session.selectOne("driver.selectDriver", memberNum);
 	}
+
+	@Override
+	public int enrollDriver(Map<String, Object> driver) {
+		return session.insert("driver.enrollDriver",driver);
+	}
+
+	@Override
+	public int insertCarImage(carImage cImg) {
+		return session.insert("driver.insertCarImage",cImg);
+	}
+
+	@Override
+	public int selectJoinCount() {
+		return session.selectOne("driver.selectJoinCount");
+	}
+
+	@Override
+	public List<Map<String, String>> selectDriverList(int cPage, int numPerPage) {
+		RowBounds rb = new RowBounds((cPage-1)*numPerPage,numPerPage);
+		
+		return session.selectList("driver.selectDriverList",null,rb);
+	}
+	
+	
+	
+	
+	
 	
 }
