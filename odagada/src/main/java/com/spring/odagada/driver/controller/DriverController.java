03d61c6e9a63a7588ea4b51d2ecaa9df820b0e3f
gatherSong <chdbrlflfhem@naver.com>
2019-03-16 22:02:05 +0900

    드라이버 정보창 수정중

--- odagada/src/main/java/com/spring/odagada/driver/controller/DriverController.java
+++ odagada/src/main/java/com/spring/odagada/driver/controller/DriverController.java
@@ -33,231 +33,232 @@ import com.spring.odagada.member.model.vo.Member;
 
 @Controller
 public class DriverController {
-	
-	private Logger logger = LoggerFactory.getLogger(DriverController.class);
-	
-	@Autowired
-	DriverService service;
-	
-	@Autowired
-	MemberService memberService;
-	
-	@RequestMapping("/driver/driverEnroll")
-	public ModelAndView driverEnroll(HttpSession session) {
-		
-		Member m = (Member) session.getAttribute("logined");
-		ModelAndView mv = new ModelAndView();		
-		
-		
-		if(m!=null)
-		{
-			int memberNum = m.getMemberNum();		
-			Driver driver = service.selectOne(memberNum);	
-			
-			if(driver!=null)
-			{
-				mv.setViewName("/common/msg");
-				mv.addObject("msg","드라이버 등록되어있습니다.");
-				mv.addObject("loc","/");
-				
-				return mv;
-			}
-			else {
-				logger.debug("회원정보"+m);
-				logger.debug("앞자리 테스트"+m.getPhone().substring(0,3));
-				if(m.getPhone().length()==11)
-				{
-					logger.debug("나머지테스트"+m.getPhone().substring(3,11));
-				}
-				else if(m.getPhone().length()==10)
-				{
-					logger.debug("나머지테스트"+m.getPhone().substring(3,10));
-				}
-				mv.addObject("driver",driver);
-				mv.setViewName("driver/driverEnroll");
-				
-				
-				logger.debug("드라이버 있나요"+driver);
-				return mv;
-			}			
-		}
-		else {
-			mv.setViewName("/common/msg");
-			mv.addObject("msg","회원가입 후 이용해주세요.");
-			mv.addObject("loc","/member/loginForm.do");
-			
-			return mv;
-		}
-	}
-	// 파일에 대한 사이즈를 비교? 배열로 넘...ㄱ...ㅣ....ㅁ..
-	
-	@RequestMapping("/driver/driverEnrollEnd")
-	public String driverEnrollEnd(HttpServletRequest request,MultipartFile[] upFile) throws BoardException
-	{
-		int memberNum = Integer.parseInt(request.getParameter("memberNum"));
-		String memberId = request.getParameter("memberId");
-		String memberName = request.getParameter("memberName");
-		String birth = request.getParameter("birth");
-		
-		String phone1 = request.getParameter("phone1");
-		String phone2 = request.getParameter("phone2");
-		String phone = phone1+phone2;
-		
-		String licenseNum = request.getParameter("licenseNum");
-		String carModel = request.getParameter("carModel");
-		String carNum = request.getParameter("carNum");
-		String driver_info = request.getParameter("driver_info");
-		
-		int imgOrder = 0;
-		
-		logger.debug("멤버번호 테스트"+memberNum);
-		
-		
-		Map<String,Object> driver = new HashMap();
-		driver.put("memberNum", memberNum);
-		driver.put("licenseNum", licenseNum);
-		driver.put("carModel", carModel);
-		driver.put("carNum",carNum);
-		driver.put("driver_info", driver_info);
+   
+   private Logger logger = LoggerFactory.getLogger(DriverController.class);
+   
+   @Autowired
+   DriverService service;
+   
+   @Autowired
+   MemberService memberService;
+   
+   @RequestMapping("/driver/driverEnroll")
+   public ModelAndView driverEnroll(HttpSession session) {
+      
+      Member m = (Member) session.getAttribute("logined");
+      ModelAndView mv = new ModelAndView();      
+      
+      
+      if(m!=null)
+      {
+         int memberNum = m.getMemberNum();      
+         Driver driver = service.selectOne(memberNum);   
+         
+         if(driver!=null)
+         {
+            mv.setViewName("/common/msg");
+            mv.addObject("msg","드라이버 등록되어있습니다.");
+            mv.addObject("loc","/");
+            
+            return mv;
+         }
+         else {
+            logger.debug("회원정보"+m);
+            logger.debug("앞자리 테스트"+m.getPhone().substring(0,3));
+            if(m.getPhone().length()==11)
+            {
+               logger.debug("나머지테스트"+m.getPhone().substring(3,11));
+            }
+            else if(m.getPhone().length()==10)
+            {
+               logger.debug("나머지테스트"+m.getPhone().substring(3,10));
+            }
+            mv.addObject("driver",driver);
+            mv.setViewName("driver/driverEnroll");
+            
+            
+            logger.debug("드라이버 있나요"+driver);
+            return mv;
+         }         
+      }
+      else {
+         mv.setViewName("/common/msg");
+         mv.addObject("msg","회원가입 후 이용해주세요.");
+         mv.addObject("loc","/member/loginForm.do");
+         
+         return mv;
+      }
+   }
+   // 파일에 대한 사이즈를 비교? 배열로 넘...ㄱ...ㅣ....ㅁ..
+   
+   @RequestMapping("/driver/driverEnrollEnd")
+   public String driverEnrollEnd(HttpServletRequest request,MultipartFile[] upFile) throws BoardException
+   {
+      int memberNum = Integer.parseInt(request.getParameter("memberNum"));
+      String memberId = request.getParameter("memberId");
+      String memberName = request.getParameter("memberName");
+      String birth = request.getParameter("birth");
+      
+      String phone1 = request.getParameter("phone1");
+      String phone2 = request.getParameter("phone2");
+      String phone = phone1+phone2;
+      
+      String licenseNum = request.getParameter("licenseNum");
+      String carModel = request.getParameter("carModel");
+      String carNum = request.getParameter("carNum");
+      String driver_info = request.getParameter("driver_info");
+      
+      int imgOrder = 0;
+      
+      logger.debug("멤버번호 테스트"+memberNum);
+      
+      
+      Map<String,Object> driver = new HashMap();
+      driver.put("memberNum", memberNum);
+      driver.put("licenseNum", licenseNum);
+      driver.put("carModel", carModel);
+      driver.put("carNum",carNum);
+      driver.put("driver_info", driver_info);
 
-		
-		ArrayList<CarImage> files = new ArrayList();
-		
-		String savDir = request.getSession().getServletContext().getRealPath("/resources/upload/car");
-		
-		for(MultipartFile f : upFile) {
-		
-			//파일명을 생성(rename)
-			String oriFileName = f.getOriginalFilename();
-			String ext = oriFileName.substring(oriFileName.lastIndexOf("."));
-			//rename 규칙설정
-			SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmssSSS");
-			int rdv = (int)(Math.random()*1000);
-			String reName=sdf.format(System.currentTimeMillis())+"_"+rdv+ext;
-			//파일을 저장해보쟈
-			try {
-				f.transferTo(new File(savDir+"/"+reName));
-			}catch(IllegalStateException | IOException e)
-			{
-				e.printStackTrace();
-			}
-			imgOrder = imgOrder+1;
-			CarImage cImg = new CarImage();
-			cImg.setCarImageOri(oriFileName);
-			cImg.setCarImageRe(reName);
-			logger.debug("자동차 번호 "+carNum);
-			cImg.setCarNum(carNum);
-			cImg.setImageOrder(imgOrder);
-			files.add(cImg);
+      
+      ArrayList<CarImage> files = new ArrayList();
+      
+      String savDir = request.getSession().getServletContext().getRealPath("/resources/upload/car");
+      
+      for(MultipartFile f : upFile) {
+      
+         //파일명을 생성(rename)
+         String oriFileName = f.getOriginalFilename();
+         String ext = oriFileName.substring(oriFileName.lastIndexOf("."));
+         //rename 규칙설정
+         SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmssSSS");
+         int rdv = (int)(Math.random()*1000);
+         String reName=sdf.format(System.currentTimeMillis())+"_"+rdv+ext;
+         //파일을 저장해보쟈
+         try {
+            f.transferTo(new File(savDir+"/"+reName));
+         }catch(IllegalStateException | IOException e)
+         {
+            e.printStackTrace();
+         }
+         imgOrder = imgOrder+1;
+         CarImage cImg = new CarImage();
+         cImg.setCarImageOri(oriFileName);
+         cImg.setCarImageRe(reName);
+         logger.debug("자동차 번호 "+carNum);
+         cImg.setCarNum(carNum);
+         cImg.setImageOrder(imgOrder);
+         files.add(cImg);
 
-		}
-		
-		int result = service.enrollDriver(driver,files);
-		
-		logger.debug("파일 확인"+files);
-		
-		return "redirect:/";
-	}
-	
-	 @RequestMapping("/driver/driverList")
-	   public ModelAndView driverList(@RequestParam(value="cPage", required=false, defaultValue="0") int cPage,HttpSession session)
-	   {
-		   
-		  Member m = (Member) session.getAttribute("logined");
-		  ModelAndView mv = new ModelAndView();
-		  int numPerPage = 5;   
-		  int contentCount = service.selectJoinCount();	  
-		      
-	      List<Map<String,String>> list = service.selectDriverList(cPage,numPerPage);
-	      mv.addObject("pageBar",PageFactory.getPageBar(contentCount, cPage, numPerPage,"/odagada/driver/driverList"));
-	     
-	      mv.addObject("list",list);
-	      mv.setViewName("driver/driverJoinList");	      
-		      
-		      return mv;	      
-	   }
-	 
-	 @RequestMapping("/driver/driverForm")
-	 public ModelAndView driverForm(int memberNum,String carNum)
-	 {
-		 System.out.println("여깅하ㅏ하하하하ㅏㅎ"+memberNum+" :"+carNum);
-		 ModelAndView mv = new ModelAndView();
-		 Map<String,String> map = service.selectDriverOne(memberNum);
-		 List<Map<String,String>> carImg = service.selectCarImg(carNum);
-		 
-		 String a = map.get("LICENSENUM").substring(0, 3);
-		 String b = "**-******";
-		 String c = map.get("LICENSENUM").substring(13);
-		 
-		 String licenseNum = a+b+c;
-	
-		carImg.get(0).put("active", "active");
-		 
-		 mv.addObject("driver",map);
-		 mv.addObject("carImg", carImg);
-		 mv.addObject("licenseNum",licenseNum);
-		 mv.setViewName("driver/driverForm");
-		 
-		 logger.info("리턴 전"+map);
-		 
-		 return mv;
-	 }
-	@RequestMapping("/driver/driverFormEnd")
-	public String driverFormEnd(Driver driver,HttpServletRequest request,@RequestParam(value="driverStatus") String driverStatus,@RequestParam(value="memberNum") int memberNum)
-	{
-		Driver d = service.selectOne(memberNum);
-		String licenseNum = d.getLicenseNum();
-		
-		logger.debug("넘버= "+memberNum);
-		logger.debug("드라이버수락여부 = "+driverStatus);
-		logger.debug("면허 확인"+licenseNum);
-		logger.debug("멤버 = "+driver);
-		
-		Map<String,Object> map = new HashMap<String,Object>();
-			map.put("memberNum", memberNum);
-			
-		if(driverStatus.equals("N"))
-		{
-			driverStatus = "Y";
-			map.put("driverStatus", driverStatus);
-			int result = service.updateStatus(map);
-		}
-		
-		return "redirect: driverList";
-		
-	}
-	
-	@RequestMapping("driver/driverRefuse")
-	public String deleteDriver(@RequestParam(value="memberNum") int memberNum)
-	{
-		int result = service.deleteDriver(memberNum);
-		
-		return "redirect: driverList";
-	}
-	
+      }
+      
+      int result = service.enrollDriver(driver,files);
+      
+      logger.debug("파일 확인"+files);
+      
+      return "redirect:/";
+   }
+   
+    @RequestMapping("/driver/driverList")
+      public ModelAndView driverList(@RequestParam(value="cPage", required=false, defaultValue="0") int cPage,HttpSession session)
+      {
+         
+        Member m = (Member) session.getAttribute("logined");
+        ModelAndView mv = new ModelAndView();
+        int numPerPage = 5;   
+        int contentCount = service.selectJoinCount();     
+            
+         List<Map<String,String>> list = service.selectDriverList(cPage,numPerPage);
+         mv.addObject("pageBar",PageFactory.getPageBar(contentCount, cPage, numPerPage,"/odagada/driver/driverList"));
+        
+         mv.addObject("list",list);
+         mv.setViewName("driver/driverJoinList");         
+            
+         return mv;         
+      }
+    
+    @RequestMapping("/driver/driverForm")
+    public ModelAndView driverForm(int memberNum,String carNum)
+    {
+       System.out.println("여깅하ㅏ하하하하ㅏㅎ"+memberNum+" :"+carNum);
+       ModelAndView mv = new ModelAndView();
+       Map<String,String> map = service.selectDriverOne(memberNum);
+       List<Map<String,String>> carImg = service.selectCarImg(carNum);
+       
+       String a = map.get("LICENSENUM").substring(0, 3);
+       String b = "**-******";
+       String c = map.get("LICENSENUM").substring(13);
+       
+       String licenseNum = a+b+c;
+   
+      carImg.get(0).put("active", "active");
+       
+       mv.addObject("driver",map);
+       mv.addObject("carImg", carImg);
+       mv.addObject("licenseNum",licenseNum);
+       mv.setViewName("driver/driverForm");
+       
+       logger.info("리턴 전"+map);
+       
+       return mv;
+    }
+   @RequestMapping("/driver/driverFormEnd")
+   public String driverFormEnd(Driver driver,HttpServletRequest request,@RequestParam(value="driverStatus") String driverStatus,@RequestParam(value="memberNum") int memberNum)
+   {
+      Driver d = service.selectOne(memberNum);
+      String licenseNum = d.getLicenseNum();
+      
+      logger.debug("넘버= "+memberNum);
+      logger.debug("드라이버수락여부 = "+driverStatus);
+      logger.debug("면허 확인"+licenseNum);
+      logger.debug("멤버 = "+driver);
+      
+      Map<String,Object> map = new HashMap<String,Object>();
+         map.put("memberNum", memberNum);
+         
+      if(driverStatus.equals("N"))
+      {
+         driverStatus = "Y";
+         map.put("driverStatus", driverStatus);
+         int result = service.updateStatus(map);
+      }
+      
+      return "redirect: driverList";
+      
+   }
+   
+   @RequestMapping("driver/driverRefuse")
+   public String deleteDriver(@RequestParam(value="memberNum") int memberNum)
+   {
+      int result = service.deleteDriver(memberNum);
+      
+      return "redirect: driverList";
+   }
+   
     //드라이버 자신이 등록한 카풀 리스트 보기- 정하
     @RequestMapping("/driver/driverCarpool")
     public ModelAndView selectDriverCarpool(HttpSession session) {
-    	ModelAndView mav = new ModelAndView();
-    	Member m = (Member) session.getAttribute("logined");
-    	List<Map<String,String>> dcarList = service.selectDriverCarPool(m.getMemberNum());
-    	mav.addObject("dcarList",dcarList);
-    	mav.setViewName("member/driverCarpool");
-     	return mav;
+       ModelAndView mav = new ModelAndView();
+       Member m = (Member) session.getAttribute("logined");
+       List<Map<String,String>> dcarList = service.selectDriverCarPool(m.getMemberNum());
+       mav.addObject("dcarList",dcarList);
+       mav.setViewName("member/driverCarpool");
+        return mav;
     }
     
     //드라이버가 동승중인 이용객 보기
     @RequestMapping("/driver/selectDriverPas")
     public ModelAndView selectDriverPas(String driverNum,String carpoolNum){
-    	ModelAndView mav = new ModelAndView();
-    	Map<String,String> m = new HashMap();
-    	m.put("driverNum", driverNum);
-    	m.put("carpoolNum", carpoolNum);
-    	List<Map<String,String>> dList =service.selectDriverPas(m);
-    	mav.addObject("dList",dList);
-    	mav.setViewName("member/passengerCk");
-    	return mav; 
+       ModelAndView mav = new ModelAndView();
+       Map<String,String> m = new HashMap();
+       m.put("driverNum", driverNum);
+       m.put("carpoolNum", carpoolNum);
+       List<Map<String,String>> dList =service.selectDriverPas(m);
+       logger.debug(""+dList);
+       mav.addObject("dList",dList);
+       mav.setViewName("member/passengerCk");
+       return mav; 
     }
 
-	
+   
 }
--- odagada/src/main/webapp/WEB-INF/views/member/driverCarpool.jsp
+++ odagada/src/main/webapp/WEB-INF/views/member/driverCarpool.jsp
@@ -27,23 +27,23 @@ border-left: 7px solid rgba(180,180,180,0);
     transition-delay:0.2;
 } */
 img.logo-img{
-	padding-left:18%;
-	padding-top:20%;
-	padding-bottom:18%;
-	color:white;
+   padding-left:18%;
+   padding-top:20%;
+   padding-bottom:18%;
+   color:white;
 }
 div.my-div{
-	border-bottom:1px solid rgb(200,200,200);
+   border-bottom:1px solid rgb(200,200,200);
 }
 p.my-p{
-	color: rgb(255,255,255);
-	font-size: 22px;
-	font-family: "a어린왕자B";
+   color: rgb(255,255,255);
+   font-size: 22px;
+   font-family: "a어린왕자B";
 }
 img.my-img{
-	width:20px;
-	heigth:20px;
-	float:left;
+   width:20px;
+   heigth:20px;
+   float:left;
 }
 div.subMy-div{
 padding-top:8%;
@@ -58,162 +58,136 @@ color:white;
 box-shadow: 0px 2px 1px white,0px -2px 1px white;
 border-radius: 3px;
 transform: perspective(250px) translateZ(7px);
-}	
+}   
 .active-s{
 background-color:rgba(180,180,180,1);
 color:white;
 box-shadow: 0px 2px 1px white,0px -2px 1px white;
 border-radius: 3px;
-}	
+}   
 a.subMy-a{
-	text-decoration:none;
-	font-size: 15px;
-	font-family: "a이끌림M";
-	color:white;
+   text-decoration:none;
+   font-size: 15px;
+   font-family: "a이끌림M";
+   color:white;
 }
 button.checkPass-btn{
-	text-decoration: none;
-	border: none;
-	border-radius: 25px;
-	width:80px;
-	height:30px;
-	background-color:rgb(0,175,76);
-	color:white;
-	font-size: 16px;
-	float:right;
+   text-decoration: none;
+   border: none;
+   border-radius: 25px;
+   width:100px;
+   height:30px;
+   background-color:rgb(0,175,76);
+   color:white;
+   font-size: 16px;
+   float:right;
 }
 button.checkPass-btn:hover{
-	background-color:rgba(50,50,50,0.7);
+   background-color:rgba(50,50,50,0.7);
 }
 div.N-div{
-	border:2px solid rgb(0,175,76);
+   border:2px solid rgb(0,175,76);
+   background-color:rgba(0,175,76,0.1);
+}
+div.N-div:hover{
+box-shadow: 2px 2px 1px rgb(0,175,76),-2px -2px 1px rgb(0,175,76);
 }
 div.Y-div{
-	border:2px solid darkgray;
+   border:2px solid darkgray;
+   background-color:rgb(200,200,200);
 }
 </style>
-<script>
-function trans(){
-	alert("Sd");
-	
-}
-</script>
 <section class="container">
-	<div class="row">
- 		<div class="col-sm-12 col-md-3 mem-div">
- 			<div class="col-12">
- 				<img alt="logo" src="${path }/resources/images/mypage/logo2.png" class="img-fluid logo-img">
- 			</div>
+   <div class="row">
+       <div class="col-sm-12 col-md-3 mem-div">
+          <div class="col-12">
+             <img alt="logo" src="${path }/resources/images/mypage/logo2.png" class="img-fluid logo-img">
+          </div>
 
- 			<div class="offset-1  col-10 my-div">
- 				<p class="text-center my-p">ALL MY PAGE</p>
- 			</div>
- 			<div class="col-12 text-center subMy-div">
- 				<span>
- 					<img src="${path }/resources/images/mypage/my1.png" class="my-img">
- 					<span>
- 						<a href="#" class="subMy-a">내 정보 관리</a>
- 					</span>
- 				</span>
- 			</div>
- 			<div class="col-12 text-center subMy-div">
- 				<span>
- 					<img src="${path }/resources/images/mypage/my5.png" class="my-img">
- 					<span>
- 						<a href="${path }/member/myCarpool" class="subMy-a">내 카풀 내역</a>
- 					</span>
- 				</span>
- 			</div>
- 			<div class="col-12 text-center subMy-div">
- 				<span>
- 					<img src="${path }/resources/images/mypage/my3.png" class="my-img">
- 					<span>
- 						<a href="#" class="subMy-a">드라이버 정보 관리</a>
- 					</span>
- 				</span>
- 			</div>
- 			<div class="col-12 text-center subMy-div  active-s">
- 				<span>
- 					<img src="${path }/resources/images/mypage/my4.png" class="my-img ">
- 					<span>
- 						<a href="${path }/driver/driverCarpool" class="subMy-a">&nbsp;&nbsp;드라이버 카풀 등록 내역&nbsp;&nbsp;</a>
- 					</span>
- 				</span>
- 			</div>
-		</div> 
-		<div class="col-12 offset-md-1 col-md-7">
-			<div class="row">
-				<c:forEach items="${dcarList }" var="dc">
-					<c:if test='${dc.STATUS eq "N" }'>
-						<div class="col-md-6" style="margin-top:30px">
-							<div class="card">
-								<div class="card-body N-div">
-									<p>출발일: ${dc.STARTDATE }</p>
-									<p>출발지: ${dc.STARTCITY } ${dc.STARTDETAIL }</p>
-									<p>도착지: ${dc.ENDCITY } ${dc.ENDDETAIL }</p>
-									<span>
-										<button onclick="location.href='${path}/driver/selectDriverPas?driverNum=${logined.memberNum}&carpoolNum=${dc.CARPOOLNUM }'" class="checkPass-btn">동승자</button>
-									</span>
-								</div>
-							</div>
-						</div>
-					</c:if>
-					<c:if test='${dc.STATUS eq "Y" }'>
-						<div class="col-md-6" style="margin-top:30px">
-							<div class="card">
-								<div class="card-body Y-div">
-									<p>출발일: ${dc.STARTDATE }</p>
-									<p>출발지: ${dc.STARTCITY } ${dc.STARTDETAIL }</p>
-									<p>도착지: ${dc.ENDCITY } ${dc.ENDDETAIL }</p>
-									<span>
-										<button onclick="location.href='${path}/driver/selectDriverPas?driverNum=${logined.memberNum}&carpoolNum=${dc.CARPOOLNUM }'" class="checkPass-btn">동승자보기</button>
-									</span>
-								</div>
-							</div>
-						</div>
-					</c:if>
-				</c:forEach>
-			</div>
-		</div>
-	</div>
-	<!--번호 받기 -->
-	<div class="modal fade" id="credit" tabindex="-1" role="dialog" aria-labelledby="creditModalLabel" aria-hidden="true">
-	  <div class="modal-dialog" role="document">
-	    <div class="modal-content">
-	      <div class="modal-header">
-	        <h5 class="modal-title" id="exampleModalLabel">결제 코드 입력</h5>				        
-	        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-	          <span aria-hidden="true">&times;</span>
-	        </button>
-	      </div>
-	      <div class="modal-body" id="ttMessage-div">
-	          <div class="form-group">
-	            <label for="answer" class="form-control-label">결제 코드를 입력해주세요.</label>
-	          </div>
-              <div class="modal-body">
-		          <div class="form-group">
-		            <input type="text" class="form-control" id="credit-code" name="credit-code" style="resize: none;">
-		          </div>
-		      </div>
-	      </div>
-	      <div class="modal-footer">
-	        <button type="button" class="btn btn-secondary" data-dismiss="modal">취소</button>
-	        <button type="button" class="btn btn-primary" id="credi-ok" onclick="updateCredit();">코드 입력</button>					        
-	      </div>
-	    </div>
-	  </div>
-	</div>
+          <div class="offset-1  col-10 my-div">
+             <p class="text-center my-p">ALL MY PAGE</p>
+          </div>
+          <div class="col-12 text-center subMy-div">
+             <span>
+                <img src="${path }/resources/images/mypage/my1.png" class="my-img">
+                <span>
+                   <a href="#" class="subMy-a">내 정보 관리</a>
+                </span>
+             </span>
+          </div>
+          <div class="col-12 text-center subMy-div">
+             <span>
+                <img src="${path }/resources/images/mypage/my5.png" class="my-img">
+                <span>
+                   <a href="${path }/member/myCarpool" class="subMy-a">내 카풀 내역</a>
+                </span>
+             </span>
+          </div>
+          <div class="col-12 text-center subMy-div">
+             <span>
+                <img src="${path }/resources/images/mypage/my3.png" class="my-img">
+                <span>
+                   <a href="#" class="subMy-a">드라이버 정보 관리</a>
+                </span>
+             </span>
+          </div>
+          <div class="col-12 text-center subMy-div  active-s">
+             <span>
+                <img src="${path }/resources/images/mypage/my4.png" class="my-img ">
+                <span>
+                   <a href="${path }/driver/driverCarpool" class="subMy-a">&nbsp;&nbsp;드라이버 카풀 등록 내역&nbsp;&nbsp;</a>
+                </span>
+             </span>
+          </div>
+      </div> 
+      
+      <!-- 리스트 -->
+      <div class="col-12 offset-md-1 col-md-7">
+         <div class="row">
+            <c:forEach items="${dcarList }" var="dc">
+               <c:if test='${dc.STATUS eq "N" }'>
+                  <div class="col-md-6" style="margin-top:30px">
+                     <div class="card">
+                        <div class="card-body N-div">
+                           <p>출발일: ${dc.STARTDATE }</p>
+                           <p>출발지: ${dc.STARTCITY } ${dc.STARTDETAIL }</p>
+                           <p>도착지: ${dc.ENDCITY } ${dc.ENDDETAIL }</p>
+                           <span>
+                              <button onclick="location.href='${path}/driver/selectDriverPas?driverNum=${logined.memberNum}&carpoolNum=${dc.CARPOOLNUM }'" class="checkPass-btn">동승자 확인</button>
+                           </span>
+                        </div>
+                     </div>
+                  </div>
+               </c:if>
+               <c:if test='${dc.STATUS eq "Y" }'>
+                  <div class="col-md-6" style="margin-top:30px">
+                     <div class="card">
+                        <div class="card-body Y-div">
+                           <p>출발일: ${dc.STARTDATE }</p>
+                           <p>출발지: ${dc.STARTCITY } ${dc.STARTDETAIL }</p>
+                           <p>도착지: ${dc.ENDCITY } ${dc.ENDDETAIL }</p>
+                           <span>
+                              <button onclick="location.href='${path}/driver/selectDriverPas?driverNum=${logined.memberNum}&carpoolNum=${dc.CARPOOLNUM }'" class="checkPass-btn">동승자 결제</button>
+                           </span>
+                        </div>
+                     </div>
+                  </div>
+               </c:if>
+            </c:forEach>
+         </div>
+      </div>
+   </div>
 </section>
 <script>
-	function updateCredit(){
-		var code = $('#credit-code').val().trim();
-		var driverNum = ${logined.memberNum};
-		if(code.length==0)
-		{
-			alert('코드를 정확히 입력해주세요.');
-			return;
-		}
-		location.href="${path}/driver/updateDriverCredit?"
-	}
+   function updateCredit(){
+      var code = $('#credit-code').val().trim();
+      var driverNum = ${logined.memberNum};
+      if(code.length==0)
+      {
+         alert('코드를 정확히 입력해주세요.');
+         return;
+      }
+      location.href="${path}/driver/updateDriverCredit?"
+   }
 </script>
 <jsp:include page="/WEB-INF/views/common/footer.jsp"></jsp:include>
