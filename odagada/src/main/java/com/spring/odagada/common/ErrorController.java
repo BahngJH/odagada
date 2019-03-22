8d1fce3bbbaa96b7f85b63118a541ee76f0710ff
sinna94 <sinna94@naver.com>
2019-03-22 12:46:22 +0900

    Throwable, Execption msg 추가함

--- odagada/src/main/java/com/spring/odagada/common/ErrorController.java
+++ odagada/src/main/java/com/spring/odagada/common/ErrorController.java
@@ -27,15 +27,17 @@ public class ErrorController {
 		map.put("EXCEPTION", request.getAttribute("javax.servlet.error.exception"));
 		map.put("SERVLET_NAME", request.getAttribute("javax.servlet.error.servlet_name"));
 		try {
-			int status_code=Integer.parseInt(error_code);
+			String status_code=error_code;
 			switch(status_code) {
-			case 400: code="400"; msg="잘못된<br/> 요청입니다."; break;
-			case 403: code="403";msg="접근이<br/> 금지되었습니다."; break;
-			case 404: code="404";msg="페이지를<br/> 찾을 수 없습니다."; break;
-			case 405: code="405";msg="요청된 메소드가<br/> 허용되지 않습니다."; break;
-			case 500: code="500";msg="서버에 오류가<br/> 발생하였습니다.";break;
-			case 503: code="503";msg="서비스를<br/> 사용할 수 없습니다.";break;
-			default: msg="알 수 없는 오류가<br/> 발생하였습니다.";break;
+			case "400": code="400"; msg="잘못된<br/> 요청입니다."; break;
+			case "403": code="403"; msg="접근이<br/> 금지되었습니다."; break;
+			case "404": code="404"; msg="페이지를<br/> 찾을 수 없습니다."; break;
+			case "405": code="405"; msg="요청된 메소드가<br/> 허용되지 않습니다."; break;
+			case "500": code="500"; msg="서버에 오류가<br/> 발생하였습니다."; break;
+			case "503": code="503"; msg="서비스를<br/> 사용할 수 없습니다."; break;
+			case "Throwable" : msg="예외가<br/> 발생했습니다."; break;
+			case "Exception" : msg="예외가<br/> 발생했습니다."; break;
+			default: msg="알 수 없는 오류가<br/> 발생하였습니다."; break;
 			}
 		} catch(Exception e)
 		{
